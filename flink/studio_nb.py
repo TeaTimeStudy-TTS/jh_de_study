f70230af10cc049156252c8e873081b386dc389e
codingpoppy94 <60597797+codingpoppy94@users.noreply.github.com>
2026-04-17 10:16:04 +0900

    feat : 주석추가

--- flink/studio_nb.py
+++ flink/studio_nb.py
@@ -1,6 +1,12 @@
 %flink.pyflink
 
 # st_env => 자체적으로 준비 되어 있는 객체 사용
+'''
+setting = EnvironmentSettings.new_instance().in_streaming_mode().build()
+# SQL과 유사한 방식으로 데이터를 다룰수 있는 객체
+st_env = TableEnvironment.create( setting )
+'''
+
 # 1. 입력 테이블 정의
 t_env.execute_sql("""
     create table stock_input (
