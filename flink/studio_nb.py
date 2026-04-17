041adba9d4126ec4c0aa7b751a3e5cc19801a351
codingpoppy94 <60597797+codingpoppy94@users.noreply.github.com>
2026-04-17 10:14:43 +0900

    feat: 재플린

---
+++ flink/studio_nb.py
@@ -0,0 +1,45 @@
+%flink.pyflink
+
+# st_env => 자체적으로 준비 되어 있는 객체 사용
+# 1. 입력 테이블 정의
+t_env.execute_sql("""
+    create table stock_input (
+            ticker STRING,
+            price DOUBLE,
+            event_time TIMESTAMP(3),
+            WATERMARK FOR event_time AS event_time - INTERVAL '1' SECOND
+        ) with (
+            "connector" = "kinesis",
+            "stream"    = "de-ai-01-an2-kds-stock-input",
+            "aws.region"= "ap-northeast-2",
+            "scan.stream.initpos" = "LATEST",
+            "format"    = "json"
+        )
+        """)
+
+# 2. 출ㄹ 테이블 정의
+t_env.execute_sql("""
+        create table stock_output (
+            ticker STRING,
+            avg_price DOUBLE,
+            avg_time TIMESTAMP(3)
+        ) with (
+            "connector" = "kinesis",
+            "stream"    = "de-ai-01-an2-kds-stock-output",
+            "aws.region"= "ap-northeast-2",            
+            "format"    = "json"
+        )
+    """)
+
+# 3. 연산처리 (10초 단위로 데이터를 그룹화, 평균집계)
+t_env.execute_sql("""
+        INSERT INTO stock_output
+        SELECT
+            ticker,
+            AVG(price) as avg_price,
+            TUMBLE_END(event_time, INTERVAL '10' SECOND)  as avg_time
+        from
+            stock_input
+        GROUP BY TUMBLE(event_time, INTERVAL '10' SECOND), ticker
+                      
+    """)
